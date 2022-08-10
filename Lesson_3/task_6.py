4e2e5667c320e9042cceb27d5670ad58d4f0b329
bohdansavchuk <bohdan.savchuk@outlook.com>
2022-08-10 22:45:21 +0200

    init

---
+++ Lesson_3/task_6.py
@@ -0,0 +1,15 @@
+#First cell
+a = int(input('Enter x for 1st cell:'))
+b = int(input('Enter y for 1st cell:'))
+
+#Seconf cell
+aa = int(input('Enter x for 2nd cell:'))
+bb = int(input('Enter y for 2nd cell:'))
+
+#Formula
+x = abs(a - aa)
+y = abs(b - bb)
+if x == 1 and y == 2 or x == 2 and y == 1:
+    print('YES')
+else:
+    print('NO')

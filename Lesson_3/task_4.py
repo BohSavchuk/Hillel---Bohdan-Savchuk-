6f5731d2ab09d4b89c05096a220ffe6d71c12034
bohdansavchuk <bohdan.savchuk@outlook.com>
2022-08-13 07:56:41 +0200

    init

---
+++ Lesson_3/task_4.py
@@ -0,0 +1,10 @@
+list_of_six  = [100, 106, 112, 118, 124, 130, 136, 142, 148, 154, 160, 166, 172, 178, 184, 190, 196]
+
+for i in range (list_of_six ):
+    if i % 5:
+        print(i)
+#for(ist_of_six  i = 0; i < numbers.length; i++) {
+#    if(i % 3){
+#       console.log(numbers[i]);
+ #   }
+#}
---
+++ Lesson_3/task_5.py
@@ -0,0 +1,14 @@
+import random
+random = random.randint(1, 10)
+
+x = int(input("Please enter number from 1 to 10:"))
+
+while True:
+
+
+if x == b:
+    print('You won')
+if x != b:
+    print('You lose')
+
+

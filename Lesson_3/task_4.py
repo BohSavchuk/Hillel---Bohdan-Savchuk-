62c9c50e06ccd28a36f196351d424ebc343c2f43
bohdansavchuk <bohdan.savchuk@outlook.com>
2022-08-15 20:56:32 +0200

    init

--- Lesson_3/task_4.py
+++ Lesson_3/task_4.py
@@ -1,10 +1,6 @@
-list_of_six  = [100, 106, 112, 118, 124, 130, 136, 142, 148, 154, 160, 166, 172, 178, 184, 190, 196]
+list_of_six = [100, 106, 112, 118, 124, 130, 136, 142, 148, 154, 160, 166, 172, 178, 184, 190, 196]
 
-for i in range (list_of_six ):
-    if i % 5:
-        print(i)
-#for(ist_of_six  i = 0; i < numbers.length; i++) {
-#    if(i % 3){
-#       console.log(numbers[i]);
- #   }
-#}
+for value in list_of_six:
+    if value % 5 and value < 150:
+        print(value)
+

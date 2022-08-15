1d5b41505d91795273c410eb3fec173b04751fea
bohdansavchuk <bohdan.savchuk@outlook.com>
2022-08-15 20:52:23 +0200

    init

---
+++ Lesson_3/task_2.py
@@ -0,0 +1,4 @@
+number = input('Please enter your number: ')
+decimal = number.split('.')
+print(f'Your fractional number is {decimal[1]}')
+print(f'Your first number of your fractional number  {decimal [1][0]}')

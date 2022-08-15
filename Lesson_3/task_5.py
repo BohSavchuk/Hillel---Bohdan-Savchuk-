3c5888fafd80850deacb082cbb430b75d77f9fd0
bohdansavchuk <bohdan.savchuk@outlook.com>
2022-08-15 21:23:50 +0200

    init

--- Lesson_3/task_5.py
+++ Lesson_3/task_5.py
@@ -1,14 +1,17 @@
 import random
-random = random.randint(1, 10)
+guesses = 0
+random_number = random.randint(1, 10)
 
-x = int(input("Please enter number from 1 to 10:"))
+while guesses < 3:
+    guess = int(input("Please enter number from 1 to 10:"))
+    guesses += 1
+    if guess != random and guesses < 3:
+        print('Wrong, try one more time')
+    if guess == random:
+        break
 
-while True:
-
-
-if x == b:
+if guess == random_number:
     print('You won')
-if x != b:
-    print('You lose')
-
+else:
+    print('You lost, the correct number is {0}'.format(random_number))
 

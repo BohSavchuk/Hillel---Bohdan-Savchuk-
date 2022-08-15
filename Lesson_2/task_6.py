a8ba67f8f502bdc9f3cb3da150a5d361c297951b
bohdansavchuk <bohdan.savchuk@outlook.com>
2022-08-15 20:13:23 +0200

    init

--- Lesson_2/task_6.py
+++ Lesson_2/task_6.py
@@ -3,7 +3,7 @@ tuple_1 = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
 #Check if element x is present
 x = float(input('Enter x:'))
 
-if x in tuple_1:
+if abs(x) in tuple_1:
     print(abs(x), 'X is in the following list ', tuple_1)
 else:
     print(abs(x), 'is not in the following list', tuple_1)

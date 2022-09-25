a910bcc3aa564d4b201fdf0dea9aec3b35920f05
bohdansavchuk <bohdan.savchuk@outlook.com>
2022-09-26 00:26:53 +0200

    init

---
+++ Lesson_6/Lesson_6.py
@@ -0,0 +1,11 @@
+import inspect
+from typing import Callable
+import functools
+
+import os
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders

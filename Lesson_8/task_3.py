54ff73153d320bbd1889f9dc0d15a9243cbda546
bohdansavchuk <bohdan.savchuk@outlook.com>
2022-09-26 19:48:51 +0200

    init

--- Lesson_8/task_3.py
+++ Lesson_8/task_3.py
@@ -1,1 +1,18 @@
-print(dict(enumerate(['a', 'b', 'c', 'd', 'e'])))
+from typing import Dict, Any
+
+
+def enumarate_task3(value: dict):
+    """
+    Function return indexed dict.
+    Args:
+        value:
+
+    Returns:
+
+    """
+    result: dict[Any, Any] = dict(enumerate(value))
+    return result
+
+
+print(enumarate_task3(['a', 'b', 'c', 'd', 'e']))
+

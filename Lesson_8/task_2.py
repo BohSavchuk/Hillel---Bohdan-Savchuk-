6ca153f48765625771c7b25cd8d58e71b07847bf
bohdansavchuk <bohdan.savchuk@outlook.com>
2022-09-26 20:18:56 +0200

    init

--- Lesson_8/task_2.py
+++ Lesson_8/task_2.py
@@ -2,6 +2,11 @@ import json
 import time
 
 def track_duration_task_2():
+    """
+    Functions help to found the time duration of the Back in Black album from AC/DC
+    Returns:
+
+    """
     track_duration = 0
     with open('acdc.json', 'r') as file:
         album = json.load(file)
